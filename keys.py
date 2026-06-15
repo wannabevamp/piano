49338f65ff74c18227353fdf815cc66dfe113d03
qvxslz <2012omkrupin@gmail.com>
2026-06-15 16:17:11 +0300

    Update keys.py

--- keys.py
+++ keys.py
@@ -1,1 +1,41 @@
+from pygame import Rect, transform, image
+from effects import draw_key_effect, spawn_flying_note, update_and_draw_flying_notes
 
+KEY_UNPRESSED = transform.scale(image.load("assets/images/key_unpressed.pmg", (100, 250))
+
+NOT_BY_INDEX = {
+  0: 'C'
+  1: 'B'
+  2: 'E'
+  3: 'F'
+  4: 'G'
+}
+
+_PREV_PRESSED = set()
+
+def draw_keys(screen, key_rects, pressed_rects):
+  global _PREV_PRESSED
+  pressed_set = set(pressed_keys)
+
+  for i, rect in enumerate(key_rects):
+    is_pressed = i in pressed_set
+    screen.blit(KEY_UNPRESSED, (rect.x, rect.y))
+
+    if is_pressed and i not in _PREV_PRESED:
+      spawn_flying_note(rect, NOTE_BY_INDEX.get(1))
+
+  _PREV_PRESSED = pressed_set
+  update_and_draw_flying_notes(screen)
+
+def create_key_rects(num_keys, start_x=50, start_y= 100, key_width=100, key_height=250):
+  rects = []
+  for i in range(num_keys):
+    x = start_x + i * key_width
+    rects.append(Rect(x, start_y, key_width, key_height))
+  return rects
+
+
+
+
+
+    
