62304815705f9b6b19a8f470dc4700ab0726d6be
qvxslz <2012omkrupin@gmail.com>
2026-06-15 16:19:11 +0300

    Update sounds.py

--- sounds.py
+++ sounds.py
@@ -1,1 +1,8 @@
+from pygame import mixer
+
+def load_sounds(keys):
+  sounds = {}
+  for key, filename in keys.items():
+    sounds[key] = mixer.Sound(f"assets/sounds/{filename}")
+  return sounds
 
